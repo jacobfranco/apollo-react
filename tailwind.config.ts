dc8c7b80cdd7f2ea0fad5dfc855554c4a33a4f7f
Jacob Franco <jacobfranco@protonmail.ch>
2024-11-05 15:30:18 -0500

    update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,28 @@
-import aspectRatioPlugin from '@tailwindcss/aspect-ratio';
-import formsPlugin from '@tailwindcss/forms';
-import typographyPlugin from '@tailwindcss/typography';
-import { type Config } from 'tailwindcss';
+import aspectRatioPlugin from "@tailwindcss/aspect-ratio";
+import formsPlugin from "@tailwindcss/forms";
+import typographyPlugin from "@tailwindcss/typography";
+import { type Config } from "tailwindcss";
 
-import { parseColorMatrix } from './tailwind/colors';
+import { parseColorMatrix } from "./tailwind/colors";
 
 const config: Config = {
-  content: ['./src/**/*.{html,js,ts,tsx}', './index.html'],
-  darkMode: 'class',
+  content: ["./src/**/*.{html,js,ts,tsx}", "./index.html"],
+  darkMode: "class",
   theme: {
     screens: {
-      sm: '581px',
-      md: '768px',
-      lg: '976px',
-      xl: '1280px',
+      sm: "581px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
     },
     extend: {
       boxShadow: ({ theme }) => ({
-        '3xl': '0 25px 75px -15px rgba(0, 0, 0, 0.25)',
-        'inset-ring': `inset 0 0 0 2px ${theme('colors.primary.500')}`,
+        "3xl": "0 25px 75px -15px rgba(0, 0, 0, 0.25)",
+        "inset-ring": `inset 0 0 0 2px ${theme("colors.primary.500")}`,
       }),
 
-
       fontSize: {
-        base: '0.9375rem',
+        base: "0.9375rem",
         "7xs": "6px",
         "5xs": "8px",
         "3xs": "10px",
@@ -35,36 +34,32 @@ const config: Config = {
         inherit: "inherit",
       },
       fontFamily: {
-        'sans': [
-          'D-DIN',
-          'Soapbox i18n',
-          'Inter',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
+        sans: [
+          "D-DIN",
+          "Soapbox i18n",
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
         ],
-        'mono': [
-          'Roboto Mono',
-          'ui-monospace',
-          'mono',
-        ],
+        mono: ["Roboto Mono", "ui-monospace", "mono"],
       },
       borderRadius: {
-        '5px': '5px',
+        "5px": "5px",
       },
       spacing: {
-        '4.5': '1.125rem',
+        "4.5": "1.125rem",
       },
       colors: parseColorMatrix({
         // Define color matrix (of available colors)
@@ -74,52 +69,48 @@ const config: Config = {
         success: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
         danger: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
         accent: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
-        'accent-blue': true,
-        'gradient-start': true,
-        'gradient-end': true,
-        'greentext': true,
+        "accent-blue": true,
+        "gradient-start": true,
+        "gradient-end": true,
+        greentext: true,
       }),
       animation: {
-        'sonar-scale-4': 'sonar-scale-4 3s linear infinite',
-        'sonar-scale-3': 'sonar-scale-3 3s 0.5s linear infinite',
-        'sonar-scale-2': 'sonar-scale-2 3s 1s linear infinite',
-        'sonar-scale-1': 'sonar-scale-1 3s 1.5s linear infinite',
-        'enter': 'enter 200ms ease-out',
-        'leave': 'leave 150ms ease-in forwards',
+        "sonar-scale-4": "sonar-scale-4 3s linear infinite",
+        "sonar-scale-3": "sonar-scale-3 3s 0.5s linear infinite",
+        "sonar-scale-2": "sonar-scale-2 3s 1s linear infinite",
+        "sonar-scale-1": "sonar-scale-1 3s 1.5s linear infinite",
+        enter: "enter 200ms ease-out",
+        leave: "leave 150ms ease-in forwards",
       },
       keyframes: {
-        'sonar-scale-4': {
-          from: { opacity: '0.4', transform: 'scale(1)' },
-          to: { opacity: '0', transform: 'scale(4)' },
+        "sonar-scale-4": {
+          from: { opacity: "0.4", transform: "scale(1)" },
+          to: { opacity: "0", transform: "scale(4)" },
         },
-        'sonar-scale-3': {
-          from: { opacity: '0.4', transform: 'scale(1)' },
-          to: { opacity: '0', transform: 'scale(3.5)' },
+        "sonar-scale-3": {
+          from: { opacity: "0.4", transform: "scale(1)" },
+          to: { opacity: "0", transform: "scale(3.5)" },
         },
-        'sonar-scale-2': {
-          from: { opacity: '0.4', transform: 'scale(1)' },
-          to: { opacity: '0', transform: 'scale(3)' },
+        "sonar-scale-2": {
+          from: { opacity: "0.4", transform: "scale(1)" },
+          to: { opacity: "0", transform: "scale(3)" },
         },
-        'sonar-scale-1': {
-          from: { opacity: '0.4', transform: 'scale(1)' },
-          to: { opacity: '0', transform: 'scale(2.5)' },
+        "sonar-scale-1": {
+          from: { opacity: "0.4", transform: "scale(1)" },
+          to: { opacity: "0", transform: "scale(2.5)" },
         },
         enter: {
-          from: { transform: 'scale(0.9)', opacity: '0' },
-          to: { transform: 'scale(1)', opacity: '1' },
+          from: { transform: "scale(0.9)", opacity: "0" },
+          to: { transform: "scale(1)", opacity: "1" },
         },
         leave: {
-          from: { transform: 'scale(1)', opacity: '1' },
-          to: { transform: 'scale(0.9)', opacity: '0' },
+          from: { transform: "scale(1)", opacity: "1" },
+          to: { transform: "scale(0.9)", opacity: "0" },
         },
       },
     },
   },
-  plugins: [
-    aspectRatioPlugin,
-    formsPlugin,
-    typographyPlugin,
-  ],
+  plugins: [aspectRatioPlugin, formsPlugin, typographyPlugin],
 };
 
 export default config;
